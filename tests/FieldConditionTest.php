17222e9663fcb00eaec470cd3966841f753a88ab
Bo Henriksen <stumpdenmark@hotmail.com>
2015-04-16 23:37:53 +0200

    Update FieldConditionTest.php

--- tests/FieldConditionTest.php
+++ tests/FieldConditionTest.php
@@ -1,11 +1,10 @@
 <?php
-echo 'her:' . getcwd();
+
 require_once "./API/FieldCondition.php";
 class FieldConditionTest extends PHPUnit_Framework_TestCase
 {
     public function testNoOperator()
     {
-        $fc = new FieldCondition($field, $alias = null, $value = null, $operator = null, $includeInResult = true);
         $fc = new FieldCondition('testField');
         // Assert
         $this->assertEquals($fc->operator, null);
