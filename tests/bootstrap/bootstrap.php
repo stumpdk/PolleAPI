3662c1a321798845c9dadbec01c96b29741156b9
Bo Henriksen <stumpdenmark@hotmail.com>
2015-04-16 23:34:11 +0200

    Delete bootstrap.php

--- tests/bootstrap/bootstrap.php
+++
@@ -1,8 +0,0 @@
-echo 'bootstrap ran';
-echo getcwd();
-include_once('../API/FieldCondition.php');
-// Register the directory to your include files
-//AutoLoader::registerDirectory('../../API');
-
-g3ef34fe:
-;
