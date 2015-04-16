df49cc3724b8ed08a53c9f5d1cc524ae1fe7e2a3
Bo Henriksen <stumpdenmark@hotmail.com>
2015-04-16 23:49:17 +0200

    Update FieldConditionTest.php

--- tests/FieldConditionTest.php
+++ tests/FieldConditionTest.php
@@ -4,6 +4,7 @@ class FieldConditionTest extends PHPUnit_Framework_TestCase
 {
     public function testNoOperator()
     {
+        echo getcwd();
         $fc = new FieldCondition('testField');
         // Assert
         $this->assertEquals($fc->operator, null);
