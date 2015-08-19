09913ad1e96e6ecb21c731166b97d49a8367c583
Bo Henriksen <stumpdenmark@hotmail.com>
2015-08-19 21:13:58 +0200

    Update QueryBuilderTest.php

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -7,7 +7,7 @@ class QueryBuilderTest extends PHPUnit_Framework_TestCase
     public function testAcceptsNullValue()
     {
         $fc = array();
-        $fc[] = new FieldCondition('testField', '0');
+        $fc[] = new FieldCondition('testField', null, '0', '=');
         $join = 'test_table';
         
         $qb = new QueryBuilder($fc, $join);
